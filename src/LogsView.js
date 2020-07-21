6213775a4e4ac3d051ba1061b5918f5b42b61d72
Lior Ehrlich <liorehrlich@gmail.com>
2020-07-21 20:47:42 +0300

    add component LogsView to view the build logs

---
+++ src/LogsView.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Logs = () => <Wrapper></Wrapper>;
+
+const Wrapper = styled.div`
+  background-color: ${props => props.theme.secondaryBackground};
+`;
+
+export default Logs;
