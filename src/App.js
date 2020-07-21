f7fd26ccd44fda28f75606e385397603f9f84e5b
Lior Ehrlich <liorehrlich@gmail.com>
2020-07-21 20:49:01 +0300

    add content to app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import { ThemeProvider, createGlobalStyle } from "styled-components";
 import styled from "styled-components";
 
+import logs from "./logs.json"
 import theme from "./theme";
+import BuildOverview from "./BuildOverview";
+import LogsView from "./LogsView";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -11,13 +14,19 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const App = () => (
+const App = () => {
+  console.log(logs.status)
+  return (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
 
     <Title>Build Progress View </Title>
+
+    <BuildOverview buildStatus={logs.status} />
+
+    <LogsView />
   </ThemeProvider>
-);
+)};
 
 const Title = styled.div`
   background-color: ${props => props.theme.primary};
@@ -27,6 +36,7 @@ const Title = styled.div`
   padding: 0 20px;
   display: flex;
   align-items: center;
+  font-size: 20px;
 `;
 
 export default App;
