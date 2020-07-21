a2051186523b3cb965da5f974a944ed540b2f2b7
Lior Ehrlich <liorehrlich@gmail.com>
2020-07-21 20:06:06 +0300

    add a topbar to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React from "react";
+import { ThemeProvider, createGlobalStyle } from "styled-components";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div>
-    log viewer
-    </div>
-  );
-}
+import theme from "./theme";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: arial;
+  }
+`;
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+
+    <Title>Build Progress View </Title>
+  </ThemeProvider>
+);
+
+const Title = styled.div`
+  background-color: ${props => props.theme.primary};
+  height: 60px;
+  color: ${props => props.theme.background};
+  font-weight: bold;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+`;
 
 export default App;
