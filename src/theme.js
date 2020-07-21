08f7f4db608d33a5966678f961043408e5345913
Lior Ehrlich <liorehrlich@gmail.com>
2020-07-21 20:46:36 +0300

    add colors to theme

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,9 @@ const theme = {
   secondary: "#00897B",
   background: "#ffffff",
   secondaryBackground: "#F5FAF9",
+  success: "#11b560",
+  fail: "#b51114",
+  pending: "#e4e015"
 };
 
 export default theme;
