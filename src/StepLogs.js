8a36b921d28acea9b38169dc6873f5f9f8c55fda
Lior Ehrlich <lior.e@500tech.com>
2020-07-22 15:26:27 +0300

    stream logs asynchronously into the StepLogs.js component

--- src/StepLogs.js
+++ src/StepLogs.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
-const StepLogs = ({logs}) => (
-    <>{logs.map((log, index) => <Log key={`${log} ${index}`}>{log}</Log>)}</>
-);
+import streamLogs from "./utils/mockStreamLogsUtil";
+
+const StepLogs = ({ logs }) => {
+  const [shownLogs, setShownLogs] = useState([]);
+  const [newLog, setNewLog] = useState("");
+
+  useEffect(function simulateLogsStream() {
+    streamLogs(logs, setNewLog);
+  }, [logs]);
+
+  useEffect(
+    function updateShownLogs() {
+      if (newLog) {
+        setShownLogs([...shownLogs, newLog]);
+      }
+    },
+    [newLog]
+  );
+
+  return (
+    <>
+      {shownLogs.map((log, index) => (
+        <Log key={`${log} ${index}`}>{log}</Log>
+      ))}
+    </>
+  );
+};
 
 const Log = styled.div`
   color: dimgrey;
   margin-bottom: 5px;
+  padding: 5px;
 `;
 
 export default StepLogs;
