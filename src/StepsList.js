cd63e1dc0fcd8f3cda124e28c44aed2ad0ec732f
Lior Ehrlich <lior.e@500tech.com>
2020-07-22 17:14:33 +0300

    show steps in an order, and not all together

--- src/StepsList.js
+++ src/StepsList.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import styled from "styled-components";
 
 import Step from "./Step";
 
-const StepsList = ({ stepsList }) => (
-  <Wrapper>
-    {stepsList.map((step) => (
-      <Step step={step} key={step.name}/>
-    ))}
-  </Wrapper>
-);
+const StepsList = ({ stepsList, currentStep, setCurrentStep }) => {
+  const [shownSteps, setShownSteps] = useState([]);
+
+  useEffect(function updateShownSteps() {
+    setShownSteps([...shownSteps, stepsList[currentStep] ])
+  },[currentStep])
+
+  return (
+    <Wrapper>
+      {shownSteps.map((step) => (
+        <Step
+          step={step}
+          key={step.name}
+          onStepComplete={() => setCurrentStep(currentStep + 1)}
+        />
+      ))}
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.div`
   padding: 40px;
